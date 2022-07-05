28a606517eb4669e600678c4ca5177d88811b495
Pushpendra Jaat <57589014+PushpendraJaat@users.noreply.github.com>
2022-07-05 19:36:59 +0530

    Create 0-1 pattern.cpp

---
+++ 0-1 pattern.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"0-1 Pattern"<<endl<<"n"<<endl;
+    cin>>n;
+   
+  
+    for(int i=1; i<=n ; i++){
+        for(int j=1; j<=i; j++){
+           
+            if((j+i)%2==0){
+              
+            cout<<1<<" ";
+         
+            }
+            else{
+               
+            cout<<0<<" ";
+          
+            }
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
